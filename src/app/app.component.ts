2dd2612e71d00c3267bd311d724edc7e87e02c9e
Nicolas Miguenz <nmiguenz@soluciones.com.ar>
2021-03-18 21:47:39 -0300

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mi-proyecto-uno';
+  title = 'Ejercicio 1 - Edades';
+  numero1:number = 0;
+  numero2:number = 0;
+  suma = 0 ;
+  promedio = 0;
+
+  Calcular(){
+    if(this.numero1 !=null && this.numero2 !=null){
+      this.suma = +this.numero1 + +this.numero2;
+      this.promedio = this.suma / 2;
+    }
+  }
+
+  CleanInput(){
+    this.numero1 = 0;
+    this.numero2 = 0;
+    this.promedio = 0;
+    this.suma = 0;
+  }
+  
 }

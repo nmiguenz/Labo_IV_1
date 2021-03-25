cacad0864b2f1e2b6ea09da5132006475d678799
Nicolas Miguenz <nmiguenz@soluciones.com.ar>
2021-03-25 19:42:52 -0300

    Agrego ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
+import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{path:'',component:BienvenidoComponent},
+                        {path:'login',component:LoginComponent},
+                        {path:'**',component:ErrorComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
